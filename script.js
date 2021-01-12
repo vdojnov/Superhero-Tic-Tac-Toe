10961e06a24d8d52d378a60cb5d94808b966a5e8
Viktor Dojnov <24561451+vdojnov@users.noreply.github.com>
2021-01-11 22:35:57 -0800

    enclose to eliminate global variables

--- script.js
+++ script.js
@@ -1,50 +1,53 @@
-const Gameboard  = (() => {
-    let gameboard = []
-    const getGameboard = () => {
-        console.log(gameboard)
-    }
-    return {getGameboard}
-})();
+(function() {
 
-const displayController = (() => {
+    const Gameboard  = (() => {
+        let gameboard = []
+        const getGameboard = () => {
+            console.log(gameboard)
+        }
+        return {getGameboard}
+    })();
     
-    
-    const initiatePlayer = () => {
-
-    }
-})(); 
-
-const Player = (playerName, playerSymbol) => {
-    const name = playerName;
-    const symbol = playerSymbol;
-    const getPlayerName = () => name;
-    const getPlayerSymbol = () => symbol;
-
-    return {
-        getPlayerName, 
-        getPlayerSymbol
-    }
-};
-
-const HumanPlayer = (playerName, playerSymbol) => {    
-    const prototype = Player(playerName,playerSymbol)
-    const playTurn = () => {
+    const displayController = (() => {
         
-    }
-    const returnObj = {playTurn}
-    return Object.assign({},prototype,returnObj)
-};
-
-
-
-
-
-
-
-
-
-
-
+        
+        const initiatePlayer = () => {
+            
+        }
+    })(); 
+    
+    const Player = (playerName, playerSymbol) => {
+        const name = playerName;
+        const symbol = playerSymbol;
+        const getPlayerName = () => name;
+        const getPlayerSymbol = () => symbol;
+        
+        return {
+            getPlayerName, 
+            getPlayerSymbol
+        }
+    };
+    
+    const HumanPlayer = (playerName, playerSymbol) => {    
+        const prototype = Player(playerName,playerSymbol)
+        const playTurn = () => {
+            
+        }
+        const returnObj = {playTurn}
+        return Object.assign({},prototype,returnObj)
+    };
+    console.log("Hello")
+})();
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 // This is for when I implement the MiniMax Algorithm
 // const AIPlayer = () => {
 //     const prototype = Player(playerName,playerSymbol)
